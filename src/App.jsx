c847c5c01c2829f147feb9e36447c72e394a6a13
ransomenote <kyle@ransomenote.com>
2021-11-23 01:39:57 -0800

    Mostly working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import React from "react";
+import "./app.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Reports from "./pages/Reports";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" exact element={Home} />
+          <Route path="/reports" element={Reports} />
+          <Route path="/products" element={Products} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 
 export default App;
